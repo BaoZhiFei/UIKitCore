17b3f46e8540a295c6483688bceb1ee96185779e
baozhifei <baozhifei@gmail.com>
2021-11-16 21:57:25 +0800

    init import

---
+++ UIKitCore/UIKitCore.h
@@ -0,0 +1,18 @@
+//
+//  UIKitCore.h
+//  UIKitCore
+//
+//  Created by mac on 2021/11/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIKitCore.
+FOUNDATION_EXPORT double UIKitCoreVersionNumber;
+
+//! Project version string for UIKitCore.
+FOUNDATION_EXPORT const unsigned char UIKitCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKitCore/PublicHeader.h>
+
+
